99a3e0c42a4d0687e0b32a91a4f37a26f51e628d
wwwsevolod <www@sevolod.ru>
2014-06-24 17:05:39 +0400

    test

--- src/app/panels/histogram/module.js
+++ src/app/panels/histogram/module.js
@@ -539,6 +539,10 @@ function (angular, app, $, _, kbn, moment, timeSeries, numeral) {
       });
     };
 
+    $scope.alert = function(factor) {
+      alert(factor);
+    }
+
     // I really don't like this function, too much dom manip. Break out into directive?
     $scope.populate_modal = function(request) {
       $scope.inspector = angular.toJson(JSON.parse(request.toString()),true);
